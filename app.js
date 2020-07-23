42041c15412f6c1f0a8629f00a9adbddda5b5b4d
Aman <aman.srivas16@gmail.com>
2020-07-23 20:54:26 +0530

    add gitignor2

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 let port=process.env.PORT;
-if(port == null || port=""){
+if(port == null || port==""){
   port=3000;
 }
 app.listen(port, function() {
